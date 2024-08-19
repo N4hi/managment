8f21f25e1018d77e2dbdd8276b48f0ac4a520584
alnahi <alialhaqjasim@gmail.com>
2024-08-20 00:43:52 +0300

    made a few changes to gui

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-from customtkinter import *
+from tkinter import *
+from PIL import Image, ImageTk
+class IMS:
+    def __init__(self, root):
+        self.root = root
+        self.root.geometry("1350x700")
+        self.root.title("الناهي للمبيعات")
 
-app = CTk()
-app.geometry("500x400")
+        #title
+        self.icon_title=PhotoImage(file="images\logo1.png")
+        title=Label(self.root,text="الناهي للمبيعات", image=self.icon_title, compound=LEFT, font=("times new roman", 20, 'bold'),fg="blue",anchor="w", padx=20).place(x=1100, y=0, relwidth=1, height=70)
 
-set_appearance_mode("system")
 
-btn = CTkButton(master=app, text="Click", corner_radius=32, fg_color="#4158d0",
-                hover_color="#c850c0", border_color="#FFCC70",
-                border_width=2,)
-btn.place(relx=0.5, rely=0.5, anchor="center")
-
-app.mainloop()
+        btn_logout=Button(self.root,text="Logout",cursor="hand2", font=("arial", 15, 'bold'),bg='red').place(x=0,y=10,height=50,width=150)
+# Initialize the main window
+root=Tk()
+obj=IMS(root)
+root.mainloop()
